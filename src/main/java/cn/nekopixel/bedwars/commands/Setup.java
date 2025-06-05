e3715c440b30e92679eb241eeb974aa1f490ceb9
BLxcwg666 <huixcwg@gmail.com>
2025-06-06 01:55:20 +0800

    feat(setup): better setting & limit team color

--- src/main/java/cn/nekopixel/bedwars/commands/Setup.java
+++ src/main/java/cn/nekopixel/bedwars/commands/Setup.java
@@ -11,6 +11,7 @@ import java.util.*;
 
 public class Setup implements CommandExecutor {
     private final Main plugin;
+    private final Set<String> validTeams = Set.of("red", "blue", "green", "yellow", "aqua", "white", "pink", "gray");
 
     public Setup(Main plugin) {
         this.plugin = plugin;
@@ -31,33 +32,37 @@ public class Setup implements CommandExecutor {
         switch (args[0].toLowerCase()) {
             case "setspawn" -> {
                 if (args.length < 2) {
-                    sender.sendMessage("§c用法: /bw setspawn <team>");
+                    sender.sendMessage("§c用法: /bw setspawn <team> [x] [y] [z] [yaw] [pitch]");
                     return true;
                 }
                 String team = args[1].toLowerCase();
-                Location loc = p.getLocation();
+                if (!validTeams.contains(team)) {
+                    sender.sendMessage("§c无效的队伍颜色！可用颜色: " + String.join(", ", validTeams));
+                    return true;
+                }
+                Location loc = getLocationFromArgs(p, args, 2);
                 plugin.getConfig().set("spawnpoints." + team, loc.serialize());
                 sender.sendMessage("§a已设置 §e" + team + " §a队出生点！");
             }
 
             case "setnpc" -> {
                 if (args.length < 2) {
-                    sender.sendMessage("§c用法: /bw setnpc <shop|upgrade>");
+                    sender.sendMessage("§c用法: /bw setnpc <shop|upgrade> [x] [y] [z] [yaw] [pitch]");
                     return true;
                 }
                 String type = args[1].toLowerCase();
-                Location loc = p.getLocation();
+                Location loc = getLocationFromArgs(p, args, 2);
                 plugin.getConfig().set("npcs." + type, loc.serialize());
                 sender.sendMessage("§a已设置 §e" + type + " §aNPC 位置！");
             }
 
             case "setspawner" -> {
                 if (args.length < 2) {
-                    sender.sendMessage("§c用法: /bw setspawner <iron|gold|diamond|emerald>");
+                    sender.sendMessage("§c用法: /bw setspawner <type> [x] [y] [z] [yaw] [pitch]");
                     return true;
                 }
                 String type = args[1].toLowerCase();
-                Location loc = p.getLocation();
+                Location loc = getLocationFromArgs(p, args, 2);
                 List<Map<?, ?>> list = plugin.getConfig().getMapList("spawners." + type);
                 list.add(loc.serialize());
                 plugin.getConfig().set("spawners." + type, list);
@@ -78,12 +83,30 @@ public class Setup implements CommandExecutor {
         return true;
     }
 
+    private Location getLocationFromArgs(Player player, String[] args, int startIndex) {
+        if (args.length <= startIndex) {
+            return player.getLocation();
+        }
+
+        try {
+            double x = Double.parseDouble(args[startIndex]);
+            double y = Double.parseDouble(args[startIndex + 1]);
+            double z = Double.parseDouble(args[startIndex + 2]);
+            float yaw = args.length > startIndex + 3 ? Float.parseFloat(args[startIndex + 3]) : player.getLocation().getYaw();
+            float pitch = args.length > startIndex + 4 ? Float.parseFloat(args[startIndex + 4]) : player.getLocation().getPitch();
+
+            return new Location(player.getWorld(), x, y, z, yaw, pitch);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            return player.getLocation();
+        }
+    }
+
     private void sendHelp(CommandSender sender) {
         sender.sendMessage("""
                 §6[Bedwars 地图配置]
-                §e/bw setspawn <team> §7- 设置队伍出生点
-                §e/bw setnpc <shop|upgrade> §7- 设置商店/升级NPC
-                §e/bw setspawner <type> §7- 设置资源生成点
+                §e/bw setspawn <team> [x] [y] [z] [yaw] [pitch] §7- 设置队伍出生点
+                §e/bw setnpc <shop|upgrade> [x] [y] [z] [yaw] [pitch] §7- 设置商店/升级NPC
+                §e/bw setspawner <type> [x] [y] [z] [yaw] [pitch] §7- 设置资源生成点
                 §e/bw saveconfig §7- 保存配置文件
                 """);
     }
