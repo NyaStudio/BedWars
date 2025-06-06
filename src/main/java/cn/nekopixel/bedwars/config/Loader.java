25058319471e1da25371414acc626c1ada7b3ee6
BLxcwg666 <huixcwg@gmail.com>
2025-06-06 21:09:32 +0800

    perf: update Loader & Config

--- src/main/java/cn/nekopixel/bedwars/config/Loader.java
+++ src/main/java/cn/nekopixel/bedwars/config/Loader.java
@@ -1,7 +1,6 @@
 package cn.nekopixel.bedwars.config;
 
 import cn.nekopixel.bedwars.Main;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -23,12 +22,24 @@ public class Loader {
         return result;
     }
 
-    public Map<String, Location> loadNPCs() {
-        Map<String, Object> raw = plugin.getConfig().getConfigurationSection("npcs").getValues(false);
-        Map<String, Location> result = new HashMap<>();
-        for (Map.Entry<String, Object> entry : raw.entrySet()) {
-            result.put(entry.getKey().toLowerCase(), Location.deserialize((Map<String, Object>) entry.getValue()));
+    public Map<String, List<Location>> loadNPCs() {
+        Map<String, List<Location>> result = new HashMap<>();
+        FileConfiguration cfg = plugin.getConfig();
+
+        List<Map<?, ?>> shopList = cfg.getMapList("npcs.shop");
+        List<Location> shopLocations = new ArrayList<>();
+        for (Map<?, ?> map : shopList) {
+            shopLocations.add(Location.deserialize((Map<String, Object>) map));
         }
+        result.put("shop", shopLocations);
+
+        List<Map<?, ?>> upgradeList = cfg.getMapList("npcs.upgrade");
+        List<Location> upgradeLocations = new ArrayList<>();
+        for (Map<?, ?> map : upgradeList) {
+            upgradeLocations.add(Location.deserialize((Map<String, Object>) map));
+        }
+        result.put("upgrade", upgradeLocations);
+
         return result;
     }
 
@@ -46,5 +57,19 @@ public class Loader {
         }
 
         return result;
+    }
+
+    public Map<String, Location> loadBeds() {
+        Map<String, Object> raw = plugin.getConfig().getConfigurationSection("beds").getValues(false);
+        Map<String, Location> result = new HashMap<>();
+        for (Map.Entry<String, Object> entry : raw.entrySet()) {
+            result.put(entry.getKey().toLowerCase(), Location.deserialize((Map<String, Object>) entry.getValue()));
+        }
+        return result;
+    }
+
+    public Location loadLobby() {
+        Map<String, Object> raw = plugin.getConfig().getConfigurationSection("lobby").getValues(false);
+        return Location.deserialize((Map<String, Object>) raw);
     }
 }
