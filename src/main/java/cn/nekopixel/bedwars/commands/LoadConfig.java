7c57b42bf4d2b548c2ed0318fbda57ecaaf7c452
BLxcwg666 <huixcwg@gmail.com>
2025-06-07 02:57:45 +0800

    perf: improve LoadConfig.java

--- src/main/java/cn/nekopixel/bedwars/commands/LoadConfig.java
+++ src/main/java/cn/nekopixel/bedwars/commands/LoadConfig.java
@@ -2,6 +2,7 @@ package cn.nekopixel.bedwars.commands;
 
 import cn.nekopixel.bedwars.Main;
 import org.bukkit.command.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
@@ -15,15 +16,15 @@ public class LoadConfig implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!sender.hasPermission("bedwars.admin")) {
-            sender.sendMessage("§c你没有使用此命令的权限");
+            sender.sendMessage(ChatColor.RED + "你没有使用此命令的权限");
             return true;
         }
 
         try {
             plugin.reloadConfig();
-            sender.sendMessage("§a配置文件已重新加载");
+            sender.sendMessage(ChatColor.GREEN + "配置文件已重新加载");
         } catch (Exception e) {
-            sender.sendMessage("§c配置文件加载失败: " + e.getMessage());
+            sender.sendMessage(ChatColor.RED + "配置文件加载失败: " + e.getMessage());
         }
 
         return true;
