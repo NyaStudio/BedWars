f419b7c81431abd0467534ea183f0d379849431f
BLxcwg666 <huixcwg@gmail.com>
2025-06-07 16:38:19 +0800

    feat: cancel advancements

--- src/main/java/cn/nekopixel/bedwars/listener/CancelEvents.java
+++ src/main/java/cn/nekopixel/bedwars/listener/CancelEvents.java
@@ -5,9 +5,13 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
 
 public class CancelEvents implements Listener {
 
+    // cancel bed events
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (!event.hasBlock() || event.getClickedBlock() == null) return;
@@ -17,6 +21,16 @@ public class CancelEvents implements Listener {
 
         if (type.name().endsWith("_BED")) {
             event.setCancelled(true);
+        }
+    }
+
+    // cancel advancements
+    @EventHandler
+    public void onAdvancement(PlayerAdvancementDoneEvent event) {
+        Advancement advancement = event.getAdvancement();
+        AdvancementProgress progress = event.getPlayer().getAdvancementProgress(advancement);
+        for (String criterion : progress.getAwardedCriteria()) {
+            progress.revokeCriteria(criterion);
         }
     }
 }
