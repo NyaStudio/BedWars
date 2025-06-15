cbb885a95ee08f529fc016c17de6bfeb3c10312f
BLxcwg666 <huixcwg@gmail.com>
2025-06-16 00:23:21 +0800

    feat(world): implement world settings for time and weather control

--- src/main/java/cn/nekopixel/bedwars/listener/WorldEvents.java
+++ src/main/java/cn/nekopixel/bedwars/listener/WorldEvents.java
@@ -1,11 +1,53 @@
 package cn.nekopixel.bedwars.listener;
 
+import cn.nekopixel.bedwars.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.world.TimeSkipEvent;
 
 public class WorldEvents implements Listener {
+    private final Main plugin;
+    private boolean lockTime;
+    private int lockedTime;
+    private boolean disableWeather;
+    private boolean disableDaylightCycle;
+
+    public WorldEvents(Main plugin) {
+        this.plugin = plugin;
+        loadWorldSettings();
+    }
+
+    private void loadWorldSettings() {
+        lockTime = plugin.getConfig().getBoolean("world.lock_time", true);
+        lockedTime = plugin.getConfig().getInt("world.locked_time", 6000);
+        disableWeather = plugin.getConfig().getBoolean("world.disable_weather", true);
+        disableDaylightCycle = plugin.getConfig().getBoolean("world.disable_daylight_cycle", true);
+
+        for (World world : plugin.getServer().getWorlds()) {
+            if (lockTime) {
+                world.setTime(lockedTime);
+            }
+            if (disableWeather) {
+                world.setStorm(false);
+                world.setThundering(false);
+            }
+            if (disableDaylightCycle) {
+                world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            }
+        }
+
+        if (lockTime) {
+            Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+                for (World world : Bukkit.getWorlds()) {
+                    world.setTime(lockedTime);
+                }
+            }, 0L, 100L);
+        }
+    }
 
     @EventHandler
     public void onWeatherChange(WeatherChangeEvent event) {
