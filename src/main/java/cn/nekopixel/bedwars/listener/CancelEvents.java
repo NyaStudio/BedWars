23f4bcad0bb63145454a4ce77c7d75740ed895df
BLxcwg666 <huixcwg@gmail.com>
2025-06-15 23:23:28 +0800

    fix(events): cancel advancements

--- src/main/java/cn/nekopixel/bedwars/listener/CancelEvents.java
+++ src/main/java/cn/nekopixel/bedwars/listener/CancelEvents.java
@@ -1,26 +1,24 @@
 package cn.nekopixel.bedwars.listener;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 
 public class CancelEvents implements Listener {
     private final Plugin plugin;
-    private final ProtocolManager protocolManager;
 
     public CancelEvents(Plugin plugin) {
         this.plugin = plugin;
-        this.protocolManager = ProtocolLibrary.getProtocolManager();
-        advancementCanceller();
+        checkWorlds();
     }
 
     // cancel bed events
@@ -36,22 +34,43 @@ public class CancelEvents implements Listener {
         }
     }
 
-    // cancel advancements
-    // idk, maybe i cant fix this
-    private void advancementCanceller() {
-        protocolManager.addPacketListener(new PacketAdapter(plugin, PacketType.Play.Server.ADVANCEMENTS) {
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                event.setCancelled(true);
-            }
-        });
-    }
-
     // 雪豹闭嘴
     @EventHandler
     public void onVillagerSound(org.bukkit.event.entity.EntitySpawnEvent event) {
         if (event.getEntity() instanceof Villager villager) {
             villager.setSilent(true);
         }
+    }
+
+    // cancel advancements
+    // so im a baka, i fixed that
+    private void checkWorlds() {
+        for (org.bukkit.World world : Bukkit.getWorlds()) {
+            if (Boolean.TRUE.equals(world.getGameRuleValue(org.bukkit.GameRule.ANNOUNCE_ADVANCEMENTS))) {
+                world.setGameRule(org.bukkit.GameRule.ANNOUNCE_ADVANCEMENTS, false);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerAdvancement(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        int playerLevel = player.getLevel();
+        float playerExp = player.getExp();
+        Advancement advancement = event.getAdvancement();
+        
+        if (!advancement.getKey().getNamespace().contains("minecraft")) return;
+        
+        for (String criteria : advancement.getCriteria()) {
+            player.getAdvancementProgress(advancement).revokeCriteria(criteria);
+        }
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            int playerNewLevel = player.getLevel();
+            float playerNewExp = player.getExp();
+            if (playerNewExp != playerExp || playerNewLevel != playerLevel) {
+                player.setLevel(playerLevel);
+                player.setExp(playerExp);
+            }
+        }, 2L);
     }
 }
