0610d431e361aeda6071c29838b08ddab1a5d1bf
BLxcwg666 <huixcwg@gmail.com>
2025-06-28 04:25:24 +0800

    feat(version): enhance version command output with build time

--- src/main/java/cn/nekopixel/bedwars/commands/VersionCommand.java
+++ src/main/java/cn/nekopixel/bedwars/commands/VersionCommand.java
@@ -24,20 +24,24 @@ public class VersionCommand {
         String version = plugin.getDescription().getVersion();
         String gitHash = getGitHash();
         String gitBranch = getGitBranch();
+        String buildTime = getBuildTime();
         
-        sender.sendMessage(ChatColor.GOLD + "=== BedWars @ " + ChatColor.translateAlternateColorCodes('&', "&cN&6e&ek&ao&bP&9i&5x&de&cl") + " " + ChatColor.GOLD + "===");
-        sender.sendMessage(ChatColor.WHITE + "v" + version + ChatColor.GREEN + " By " + ChatColor.AQUA + String.join(", ", plugin.getDescription().getAuthors()));
+        sender.sendMessage(ChatColor.GOLD + "====== BedWars @ " + ChatColor.translateAlternateColorCodes('&', "&cN&6e&ek&ao&bP&9i&5x&de&cl") + " " + ChatColor.GOLD + "======");
+        sender.sendMessage(ChatColor.YELLOW + "v" + version + ChatColor.GREEN + " By " + ChatColor.AQUA + String.join(", ", plugin.getDescription().getAuthors()));
         if (gitHash != null && !gitHash.isEmpty() && !gitHash.contains("${")) {
-            sender.sendMessage("Branch: " +
+            sender.sendMessage(ChatColor.YELLOW + "Branch: " +
                     (gitBranch != null && !gitBranch.isEmpty() && !gitBranch.contains("${")
                             ? ChatColor.WHITE + gitBranch + ChatColor.GRAY + " (" + gitHash + ")"
                             : ChatColor.WHITE + gitHash
                     ));
         }
+        if (buildTime != null && !buildTime.isEmpty() && !buildTime.contains("${")) {
+            sender.sendMessage(ChatColor.YELLOW + "Build At: " + ChatColor.WHITE + buildTime);
+        }
         sender.sendMessage("");
         sender.sendMessage(ChatColor.GREEN + ChatColor.translateAlternateColorCodes('&', "&cN&6e&ek&ao&bP&9i&5x&de&cl") + " 社区动力！");
         sender.sendMessage(ChatColor.AQUA + "" + ChatColor.UNDERLINE + "https://www.nekopixel.cn/");
-        sender.sendMessage(ChatColor.GOLD + "==========================");
+        sender.sendMessage(ChatColor.GOLD + "==============================");
     }
     
     private static String getGitHash() {
@@ -48,5 +52,10 @@ public class VersionCommand {
     private static String getGitBranch() {
         String branch = gitProperties.getProperty("git.branch");
         return (branch != null && !branch.contains("${")) ? branch : null;
+    }
+    
+    private static String getBuildTime() {
+        String buildTime = gitProperties.getProperty("git.build.time");
+        return (buildTime != null && !buildTime.contains("${")) ? buildTime : null;
     }
 } 
